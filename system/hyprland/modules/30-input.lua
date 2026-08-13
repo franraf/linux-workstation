129cf5ff35d673749da049f507f1971946ab9c72
Rafael François Neto <francois.rafa@gmail.com>
2026-08-13 00:06:17 -0300

    feat(desktop): add Hyprland input module

---
+++ system/hyprland/modules/30-input.lua
@@ -0,0 +1,13 @@
+-- Keyboard, pointer and touchpad baseline.
+
+hl.config({
+  input = {
+    kb_layout = "br",
+    follow_mouse = 1,
+    touchpad = {
+      natural_scroll = false,
+      disable_while_typing = true,
+      tap_to_click = true,
+    },
+  },
+})
