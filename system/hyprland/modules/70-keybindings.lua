59e2ef21e74f6997438fdd8827cd5aa66497f382
Rafael François Neto <francois.rafa@gmail.com>
2026-08-13 00:19:24 -0300

    feat(desktop): add notification center keybinding

--- system/hyprland/modules/70-keybindings.lua
+++ system/hyprland/modules/70-keybindings.lua
@@ -2,3 +2,6 @@ local hl = require("hyprlang")
 
 -- Application launcher
 hl.bind("SUPER", "SPACE", "exec", "rofi -show drun")
+
+-- Notification center
+hl.bind("SUPER", "N", "exec", "swaync-client -t")
