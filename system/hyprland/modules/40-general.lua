8df3c08983985650132a5528c4e9062b12fbae51
Rafael François Neto <francois.rafa@gmail.com>
2026-08-13 00:06:26 -0300

    feat(desktop): add Hyprland general module

---
+++ system/hyprland/modules/40-general.lua
@@ -0,0 +1,11 @@
+-- General compositor behavior.
+
+hl.config({
+  general = {
+    layout = "dwindle",
+  },
+  misc = {
+    disable_hyprland_logo = true,
+    disable_splash_rendering = true,
+  },
+})
